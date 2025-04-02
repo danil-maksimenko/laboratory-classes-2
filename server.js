9deab362a420d2d74c8307a12bbe74e93c3d0cdc
Maksymenko Danylo <danilmaksimenko000@gmail.com>
2025-04-02 14:12:00 +0200

    final version

--- server.js
+++ server.js
@@ -2,34 +2,86 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const config = require("./config");
-const { requestRouting } = require("./routing/routing");
 const { getInfoLog, getErrorLog } = require("./utils/logger");
+const fs = require("fs");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
   console.log(getInfoLog(req.method, req.url));
   next();
 });
 
-app.use("/", (req, res, next) => {
-  if (req.url === "/") {
-    return res.sendFile(path.join(__dirname, "views", "home.html"));
-  }
-  next();
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "home.html"));
 });
 
-app.use("/product", (req, res, next) => {
-  return requestRouting(req, res);
+app.get("/product/add", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "add-product.html"));
 });
 
-app.use("/logout", (req, res, next) => {
-  return res.sendFile(path.join(__dirname, "views", "logout.html"));
+app.post("/product/add", (req, res) => {
+  const formData = [
+    `name: ${req.body.name}`,
+    `description: ${req.body.description}`,
+  ];
+
+  fs.writeFile("product.txt", `${formData[0]}, ${formData[1]}`, (err) => {
+    if (err) {
+      console.error(err);
+    }
+    res.redirect("/product/new");
+  });
 });
 
-app.use("/kill", (req, res, next) => {
+app.get("/product/new", (req, res) => {
+  fs.readFile("./product.txt", "utf-8", (err, data) => {
+    res.setHeader("Content-Type", "text/html");
+    res.write(`<!DOCTYPE html>
+<html>
+<head>
+  <title>Shop - Newest product</title>
+  <link rel="stylesheet" href="/css/main.css">
+</head>
+<body>
+  <div class="container">
+    <h1>Newest product</h1>
+    <nav>
+      <a href='/'>Home</a>
+      <a href='/product/add'>Add product</a>
+      <a href='/logout'>Logout</a>
+    </nav>`);
+
+    if (err) {
+      res.write(`<div class="content">
+        <p>No new products available.</p>
+      </div>`);
+    } else {
+      res.write(`<div class="content">
+        <div class="product-data">
+          <strong>New product data:</strong><br>
+          ${data}
+        </div>
+      </div>`);
+    }
+
+    res.write(`</div>
+</body>
+</html>`);
+
+    return res.end();
+  });
+});
+
+app.get("/logout", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "logout.html"));
+});
+
+app.get("/kill", (req, res) => {
   console.log("Process terminating...");
   process.exit();
 });
